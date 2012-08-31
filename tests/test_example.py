67312dd5a8d214152533ea82c22a034913dec027
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2012-08-31 00:07:01 -0400

    Check in a few text. No much, but somewhere to start.

--- tests/test_example.py
+++ tests/test_example.py
@@ -19,13 +19,20 @@ import sys
 import os.path
 import unittest
 sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), "..")))
+import tempfile
 
 from google_webfont_downloader import AboutGoogleWebfontDownloaderDialog
+from google_webfont_downloader.DownloadFont import extract_url, write_font_file
+from google_webfont_downloader.FindFonts import process_json
 
-class TestExample(unittest.TestCase):
+class TestCases(unittest.TestCase):
     def setUp(self):
         self.AboutGoogleWebfontDownloaderDialog_members = [
-        'AboutDialog', 'AboutGoogleWebfontDownloaderDialog', 'gettext', 'logger', 'logging']
+        'AboutDialog', 'AboutGoogleWebfontDownloaderDialog', 'logger', 'logging']
+        self.font_url = 'http://themes.googleusercontent.com/static/fonts/alfaslabone/v2/Qx6FPcitRwTC_k88tLPc-Yjjx0o0jr6fNXxPgYh_a8Q.ttf'
+        self.font_name = 'Alfa Slab One'
+        self.font_list = [["Abel"], ["Abril Fatface"]]
+
 
     def test_AboutGoogleWebfontDownloaderDialog_members(self):
         all_members = dir(AboutGoogleWebfontDownloaderDialog)
@@ -33,5 +40,46 @@ class TestExample(unittest.TestCase):
         public_members.sort()
         self.assertEqual(self.AboutGoogleWebfontDownloaderDialog_members, public_members)
 
-if __name__ == '__main__':    
+    def test_extract_url(self):
+        returned_url = extract_url(self.font_name)
+        self.assertEqual(self.font_url, returned_url)
+
+    def text_write_font_file(self):
+        fake_font_dir = tempfile.mkdtemp() + "-fonts"
+        font_url, font_dir, font_name = self.font_url, fake_font_dir, self.font_name
+        write_font_file(font_url, font_dir, font_name)
+        self.assertTrue(os.path.isfile(os.path.join(fake_font_dir, self.font_name)))
+
+    def test_process_json(self):
+        returned_list = process_json(fake_json_data)
+        self.assertEqual(self.font_list, returned_list)
+
+fake_json_data = """{
+ "kind": "webfonts#webfontList",
+ "items": [
+  {
+   "kind": "webfonts#webfont",
+   "family": "Abel",
+   "variants": [
+    "regular"
+   ],
+   "subsets": [
+    "latin"
+   ]
+  },
+  {
+   "kind": "webfonts#webfont",
+   "family": "Abril Fatface",
+   "variants": [
+    "regular"
+   ],
+   "subsets": [
+    "latin-ext",
+    "latin"
+   ]
+  }
+ ]
+}"""
+
+if __name__ == '__main__':
     unittest.main()
