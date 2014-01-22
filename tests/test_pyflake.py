af4751e931e3a505e8dcef7a48906c3dfc8c9626
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-01-22 17:35:46 -0500

    Make sure tests are running under python3. Also run pyflakes instead of pylint. The latter has too many issues with gi.

--- tests/test_pyflake.py
+++ tests/test_pyflake.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2012 Andrew Starr-Bochicchio <a.starr.b@gmail.com>
@@ -20,21 +20,9 @@ import subprocess
 
 class TestPylint(unittest.TestCase):
     def test_project_errors_only(self):
-        '''run pylint in error only mode
-        
-        your code may well work even with pylint errors
-        but have some unusual code'''
-        return_code = subprocess.call(["pylint", '-E', 'typecatcher'])
-        # not needed because nosetests displays pylint console output
-        #self.assertEqual(return_code, 0)
-
-    # un-comment the following for loads of diagnostics   
-    #~ def test_project_full_report(self):
-        #~ '''Only for the brave
-#~ 
-        #~ you will have to make judgement calls about your code standards
-        #~ that differ from the norm'''
-        #~ return_code = subprocess.call(["pylint", 'typecatcher'])
+        '''run pyflake'''
+        return_code = subprocess.call(["pyflakes3", 'typecatcher'])
+        return_code = subprocess.call(["pyflakes3", 'typecatcher_lib'])
 
 if __name__ == '__main__':
     'you will get better results with nosetests'
