af9d1d8185b1e71c51346c6d529bda244ddd4587
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-02-20 12:07:20 -0500

    Remove un-used indicator code.

--- typecatcher_lib/Window.py
+++ typecatcher_lib/Window.py
@@ -63,19 +63,6 @@ class Window(Gtk.ApplicationWindow):
         self.preferences_dialog = None # instance
         self.AboutDialog = None # class
 
-        # Optional application indicator support
-        # Run 'quickly add indicator' to get started.
-        # More information:
-        #  http://owaislone.org/quickly-add-indicator/
-        #  https://wiki.ubuntu.com/DesktopExperienceTeam/ApplicationIndicators
-        try:
-            from typecatcher import indicator
-            # self is passed so methods of this class can be called from indicator.py
-            # Comment this next line out to disable appindicator
-            self.indicator = indicator.new_application_indicator(self)
-        except ImportError:
-            pass
-
     def on_mnu_contents_activate(self, widget, data=None):
         show_uri(self, "help:%s" % get_help_uri())
 
